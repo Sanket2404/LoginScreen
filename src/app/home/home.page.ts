e59f15d5d30991ee427f78f1639b8a5cc072e449
Sanket Jain <sanketj24.rts@gmail.com>
2024-03-04 21:45:53 +0530

    Initial Commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,40 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { IonHeader, IonToolbar, IonTitle, IonContent,IonInput,IonCard,IonItem,IonButton,IonIcon,IonText,IonImg } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent,IonInput,IonCard,
+    IonItem,ReactiveFormsModule,IonButton,IonIcon,IonText,IonImg],
 })
 export class HomePage {
-  constructor() {}
+  form! : FormGroup;
+  isPwd :boolean = false;
+  constructor() {
+    this.initFrom();
+  }
+
+  initFrom(){
+    this.form  = new FormGroup({
+      email : new FormControl('',[Validators.required,Validators.email]),
+      password : new FormControl('',[Validators.required,Validators.minLength(8)])
+    });
+  }
+
+  togglePassword(){
+     this.isPwd = !this.isPwd;
+  }
+
+  submitForm(){
+   if(this.form.invalid){
+    this.form.markAllAsTouched();
+    return;
+   }
+   this.form.reset();
+   console.log(this.form.value);
+  }
+
 }
